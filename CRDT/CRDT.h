b2276e9985d2370b0df7866a57906305235bfc6a
Dominique d’Argent <dominique.dargent@me.com>
2017-07-24 17:15:45 +0200

    Fix Xcode project

---
+++ CRDT/CRDT.h
@@ -0,0 +1,19 @@
+//
+//  CRDT.h
+//  CRDT
+//
+//  Created by Dominique d'Argent on 24.07.17.
+//  Copyright © 2017 Dominique d'Argent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRDT.
+FOUNDATION_EXPORT double CRDTVersionNumber;
+
+//! Project version string for CRDT.
+FOUNDATION_EXPORT const unsigned char CRDTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRDT/PublicHeader.h>
+
+
